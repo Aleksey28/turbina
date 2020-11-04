d6e01ccceaea6bedb51b87d455144c01fd7ca1d8
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-11-05 02:33:09 +0400

    feat: added apadptiv in player

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 export default function Player({ song }) {
   const [play, setPlay] = React.useState(false);
+  const [minimize, setMinimize] = React.useState(true);
   const [progressMax, setProgressMax] = React.useState(0);
   const [progress, setProgress] = React.useState(0);
   const [refPlayer, setRefPlayer] = React.useState();
+  const [showText, setShowText] = React.useState(true);
 
   const handlePlay = () => {
     if (!!refPlayer.duration) {
@@ -15,6 +17,14 @@ export default function Player({ song }) {
       }
     }
     setPlay(!play);
+  };
+
+  const handleClickExtra = () => {
+    setShowText(!showText);
+  };
+
+  const handleMinMax = () => {
+    setMinimize(!minimize);
   };
 
   const handleSetRefPlayer = (ref) => {
@@ -51,22 +61,39 @@ export default function Player({ song }) {
         className={`player__btn player__btn_action_${!play ? 'play' : 'stop'}`}
         onClick={handlePlay}
       ></button>
-      <div className="song">
-        <div className="song__description">
-          <p className="song__name">{song.name}</p>
-          <p className="song__time">{formatTime(progress)}</p>
-        </div>
-        <div className="progress" onClick={handleSkipAhead}>
-          <div className="progress__bg">
-            <div
-              className="progress__bar"
-              style={{
-                width: `${(progress / progressMax) * 100}%`,
-              }}
-            ></div>
+
+      {/* Пришлось делать дополнительную обертку, т.к. при скрытии эекстра кнопки, более правая от неё начинает прыгать
+      Чтобы она не прыгала ширина предыдущего элемента не должна изменяться. */}
+      <div className="player__title">
+        <div className={`player__song ${minimize ? '' : 'player__song_small'}`}>
+          <div className="player__song-description">
+            <p className="player__song-name">{song.name}</p>
+            <p className="player__song-time">{formatTime(progress)}</p>
+          </div>
+          <div className="progress" onClick={handleSkipAhead}>
+            <div className="progress__bg">
+              <div
+                className="progress__bar"
+                style={{
+                  width: `${(progress / progressMax) * 100}%`,
+                }}
+              ></div>
+            </div>
           </div>
         </div>
+        <button
+          type="button"
+          className={`player__btn player__btn_action_extra ${minimize ? 'player__btn_hidden' : ''}`}
+          onClick={handleClickExtra}
+        >
+          {showText ? 'Текст песни' : 'Релизы'}
+        </button>
       </div>
+      <button
+        type="button"
+        className={`player__btn player__btn_action_${!minimize ? 'minimize' : 'maximize'}`}
+        onClick={handleMinMax}
+      ></button>
     </div>
   );
 }
@@ -82,10 +109,10 @@ function formatTime(s) {
   const seconds = totalSeconds - minutes * 60 - hours * 3600;
 
   if (hours) {
-    return hours + ':' + format2Number(minutes) + ':' + format2Number(seconds);
+    return hours + ':' + minutes + ':' + format2Number(seconds);
   }
 
-  return format2Number(minutes) + ':' + format2Number(seconds);
+  return minutes + ':' + format2Number(seconds);
 }
 
 function format2Number(num) {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-import track1 from '../audio/wham-last-christmas.mp3';
-import track2 from '../audio/a-ha-take-on-me.mp3';
+import track1 from '../audio/Wham_-_Last_Christmas_58001048.mp3';
+import track2 from '../audio/A-Ha_-_Take_On_Me_47994451.mp3';
 
 export const listOfSongs = [
   {
