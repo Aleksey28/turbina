f80ab7c534b625bc574a5a258d1733f9f39aa351
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-11-04 20:37:38 +0400

    load: get changes from origin develop

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,14 @@
 import '../index.css';
 import Main from './Main';
-<<<<<<< HEAD
-import Header from './Header'
-=======
+import Header from './Header';
 import Player from './Player';
 import { listOfSongs } from '../utils/constants';
->>>>>>> 5c54676... feat: added player
 
 function App() {
   return (
     <div className="page">
-<<<<<<< HEAD
       <Header />
-=======
       <Player song={listOfSongs[0]} />
->>>>>>> 5c54676... feat: added player
       <Main />
     </div>
   );
