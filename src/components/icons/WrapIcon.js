a4c7c1a27ab057d2f2e52147a0295f6250ff8e3c
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-11-15 21:13:15 +0400

    fix: icon wrap

--- src/components/icons/WrapIcon.js
+++ src/components/icons/WrapIcon.js
@@ -1,4 +1,4 @@
-function Wrap(props) {
+function WrapIcon(props) {
   return (
     <svg width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
@@ -11,4 +11,4 @@ function Wrap(props) {
   );
 }
 
-export default Wrap;
+export default WrapIcon;
