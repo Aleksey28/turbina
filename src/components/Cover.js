af79e16596eb9e11b9ef4dc0b835c3c1bc908d21
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-11-15 20:26:20 +0400

    fix: deleted default value in props addClass

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -1,6 +1,6 @@
 import cn from 'classnames';
 
-export default function Cover({ link, minimize, addClass = '' }) {
+export default function Cover({ link, minimize, addClass }) {
   return (
     <img src={link} alt="Обложка трека" className={cn('player__cover', { player__cover_hidden: minimize }, addClass)} />
   );
