68c06333499e3b83bf2e68e44299afdd0294c313
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-11-15 20:56:19 +0400

    fix: moved general functions from player to utils/functions

--- src/components/Player.js
+++ src/components/Player.js
@@ -8,6 +8,7 @@ import PlayIcon from './icons/PlayIcon';
 import StopIcon from './icons/StopIcon';
 import WrapIcon from './icons/WrapIcon';
 import UnwrapIcon from './icons/UnwrapIcon';
+import { formatTime, offsetLeft, isOverflow } from '../utils/functions';
 
 export default function Player({ onSetBlur }) {
   const refPlayer = useRef(null);
@@ -214,44 +215,3 @@ export default function Player({ onSetBlur }) {
     </div>
   );
 }
-
-// Функция форматирования секунд
-function formatTime(s) {
-  if (!s && s !== 0) {
-    return '00:00';
-  }
-
-  const totalSeconds = Math.floor(s);
-  const hours = Math.floor(totalSeconds / 3600);
-  const minutes = Math.floor(totalSeconds / 60) - hours * 60;
-  const seconds = totalSeconds - minutes * 60 - hours * 3600;
-
-  if (hours) {
-    return `${hours}:${minutes}:${format2Number(seconds)}`;
-  }
-
-  return `${minutes}:${format2Number(seconds)}`;
-}
-
-// Функция форматирования до двух знаяков числа
-function format2Number(num) {
-  return ('0' + num).slice(-2);
-}
-
-// Функция подсчета расстояния до края страницы
-function offsetLeft(el) {
-  var left = 0;
-  while (el && el !== document) {
-    left += el.offsetLeft;
-    el = el.offsetParent;
-  }
-  return left;
-}
-
-// Функция проверки вместимости элемента в родительский блок
-const isOverflow = (ref) => {
-  return !!ref
-    ? ref.offsetWidth - (ref.classList.contains('marquee') ? ref.parentElement.offsetWidth : 0) >
-        ref.parentElement.offsetWidth
-    : false;
-};
